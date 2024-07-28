cd6b8f734e2a07514543c30ab42dbe1a6205e416
priya4-hari <haripriyaandavar2001@gmail.com>
2024-07-28 22:05:08 +0530

    Parallel Execution In TestNG

---
+++ ParallelExecutionInTestNG/src/test/java/ParallelExe/Case3.java
@@ -0,0 +1,18 @@
+package ParallelExe;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Case3 {
+	WebDriver driver;
+	@Test
+	public void testing4()
+	{
+		driver = new ChromeDriver();
+		driver.get("https://petstore.octoperf.com/actions/Catalog.action");
+		System.out.println("Case 01 and Method 01 Thread count: "+Thread.currentThread().getId());
+		driver.close();
+	}
+
+}
